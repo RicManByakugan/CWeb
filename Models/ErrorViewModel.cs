02a4d1c2507d6fe90f7e8214c8d55ac00b776a67
Ratovonirina <eric.ratovonirina@gmail.com>
2024-01-23 07:08:16 +0300

    Initialise project

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace CWeb.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
